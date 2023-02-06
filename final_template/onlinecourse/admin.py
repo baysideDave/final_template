d38766e11e9606af90c1ecd7917fa647e99f8530
baysideDave <bhca.dave@gmail.com>
2023-02-05 23:56:36 -0500

    Feb 5 end of day

--- final_template/onlinecourse/admin.py
+++ final_template/onlinecourse/admin.py
@@ -4,6 +4,13 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
 
 class LessonInline(admin.StackedInline):
     model = Lesson
@@ -17,13 +24,13 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-class QuestionInline(admin.StackedInline):
-    model = Question
-    extra = 6
-
-
+    
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
+    
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    
 
 
 # <HINT> Register Question and Choice models here
@@ -32,5 +39,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
