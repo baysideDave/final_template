f8469e827bd7521374bc594c58ff1d1f069874c1
baysideDave <bhca.dave@gmail.com>
2023-02-11 00:59:16 -0500

    Feb 11 early morning

--- final_template/onlinecourse/models.py
+++ final_template/onlinecourse/models.py
@@ -17,7 +17,7 @@ class Instructor(models.Model):
         on_delete=models.CASCADE,
     )
     full_time = models.BooleanField(default=True)
-    total_learners = models.IntegerField()
+    total_learners = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user.username
@@ -103,11 +103,12 @@ class Enrollment(models.Model):
     # Other fields and methods you would like to design
 class Question(models.Model):
     # Foreign key to lesson
-    question_text = models.CharField(max_length=400, default="title")
+    course = models.ForeignKey(Course, null=True, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=1000, default="")
     grade = models.IntegerField(default=0)
-    lesson_id = models.ForeignKey(Lesson, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.question_text
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
@@ -126,9 +127,12 @@ class Question(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 class Choice(models.Model):
-    choice_text = models.CharField(max_length=200, default="choice")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=1000, default="choice")
     is_correct = models.BooleanField(default=False)
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
 
 
 # <HINT> The submission model
